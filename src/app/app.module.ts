ce27746985eabf4f343c173b9017cf4f5d8b1881
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-09-03 01:59:43 -0300

    creating core folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {RouterModule} from "@angular/router";
 import {Error404Component} from "./error404/error-404.component";
 import {CourseModule} from "./courses/CourseModule";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
     declarations: [
         AppComponent,
-      NavBarComponent,
       Error404Component
     ],
   imports: [
     BrowserModule,
     FormsModule,
       HttpClientModule,
+      CoreModule,
       CourseModule,   // -> importar o outro modulo q esta no curso
     RouterModule.forRoot([    //-> modulo de rotas (forRoot recebe um array de rotas)
         {
